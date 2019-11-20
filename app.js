4bc3f39491c0949820f000727ab3865194427e21
nggrekkodev <nggrekkodev@gmail.com>
2019-11-20 22:20:26 +0100

    Add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+const Idea = require('./models/Idea');
 
 const app = express();
+
+/**
+ * DATABASE CONNECTION
+ */
+mongoose
+  .connect(
+    `mongodb+srv://admin:sNc2KK8TWkSDgmxJ@cluster0-ervbx.mongodb.net/test?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log('Mongo db connected');
+  })
+  .catch(err => console.log(err));
 
 /**
  * MIDDLEWARES
@@ -15,11 +31,11 @@ app.set('view engine', 'handlebars');
  * ROUTES
  */
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.send('about');
+  res.render('about');
 });
 
 const port = 5000;
